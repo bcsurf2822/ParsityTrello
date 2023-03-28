15c7f2afeae51c4cd0ca8a8c5ec5c58f1f23ecd3
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-28 14:41:25 -0400

    able to post login and get JWT but having trouble with testing protected endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors")
+const passport = require("passport");
+
+
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+
 
 mongoose.connect("mongodb://127.0.0.1/trello2", {
   useNewUrlParser: true,
@@ -19,9 +26,51 @@ app.use(
   })
 );
 
-const mainRoutes = require("./routes/main");
+app.use(passport.initialize());
 
+//Authorization
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "trello"
+};
+
+passport.use(
+  new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+      console.log("JWT Strategy", payload)
+      const user = { myUser: 'user', myID: payload.sub }; 
+      // (payload.sub);
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+//passport use local strategy with hardcoded username and password
+passport.use(
+  "login",
+  new LocalStrategy(function (username, password, done) {
+    const authenticated = username === "trello" && password === "pass";
+
+    if (authenticated) {
+      return done(null, {myUser: "user", myId: 1234});
+    } else {
+      return done(null, false);
+    }
+  })
+);
+
+
+const mainRoutes = require("./routes/main");
 app.use(mainRoutes);
+
+const authorizedRoutes = require("./routes/login");
+app.use(authorizedRoutes);
 
 app.listen(8000, () => {
   console.log("Node.js listening on port " + 8000);
