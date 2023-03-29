d77c40668e0d7ef6925b0ddd9f0ec56a87d01852
josephl37 <josephliow2000@gmail.com>
2023-03-28 22:15:19 -0500

    added addBoard modal

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,11 +1,25 @@
 import Nav from "./nav";
 import PlusSvg from "../public/plus.svg";
+import xSvg from "../public/x-mark.svg";
+import { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const Home = () => {
-  const AddBoard = () => {
+  const [modal, toggleModal] = useState(false);
+
+  const openModal = () => toggleModal(true);
+  const closeModal = () => toggleModal(false);
+
+  const addBoardModal = () => {
+    openModal();
+  };
+
+  const addBoard = () => {
+    // TODO: get value of input
     // TODO: dispatch to addBoard
-    // TODO: new board modal should pop up
-    console.log("Added board");
+    console.log("dispatch sent!");
   };
 
   return (
@@ -19,7 +33,7 @@ const Home = () => {
         <div className="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4">
           <div
             className="rounded-lg border-2 border-dashed border-gray-300 hover:border-blue-700 h-28 flex items-center cursor-pointer"
-            onClick={AddBoard}
+            onClick={addBoardModal}
           >
             <img
               src={PlusSvg}
@@ -30,6 +44,38 @@ const Home = () => {
           </div>
           <div className="bg-gray-100 hover:bg-gray-200 rounded-lg h-28 flex items-center cursor-pointer">
             <p className="ml-4 font-bold">Title</p>
+          </div>
+
+          <div className="">
+            <Modal
+              isOpen={modal}
+              onRequestClose={closeModal}
+              className="modal w-60 bg-white border-black border rounded mx-auto mt-60"
+            >
+              <div className="mx-4">
+                <div className="flex justify-between">
+                  <p className="mt-4 font-semibold">Create Board</p>
+                  <img
+                    src={xSvg}
+                    alt="xsvg"
+                    className="object-contain w-6 mt-4 cursor-pointer hover:bg-gray-100 hover: rounded-md"
+                    onClick={closeModal}
+                  />
+                </div>
+                <label className="block mt-4">
+                  <span className="text-sm">Board Title</span>
+                  <input className="border-black border rounded mt-1 w-full"></input>
+                </label>
+                <div className="flex items-center justify-center">
+                  <button
+                    className="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 cursor-pointer font-semibold mt-6 mb-4"
+                    onClick={addBoard}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </Modal>
           </div>
         </div>
       </div>
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,17 +2,17 @@ import Logo from "../public/logo.png";
 
 const Login = () => {
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="flex items-center mb-6">
-          <img class="w-40 object-contain" src={Logo} alt="logo" />
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex items-center mb-6">
+          <img className="w-40 object-contain" src={Logo} alt="logo" />
         </div>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
                   for="email"
@@ -24,7 +24,7 @@ const Login = () => {
                   type="email"
                   name="email"
                   id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="email"
                   required=""
                 />
@@ -32,7 +32,7 @@ const Login = () => {
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Password
                 </label>
@@ -41,13 +41,13 @@ const Login = () => {
                   name="password"
                   id="password"
                   placeholder="password"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
