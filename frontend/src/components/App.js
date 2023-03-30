049f27e60fcf845a2c9a84bb996b2d377ebd8f34
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-30 10:43:09 -0400

    checkout main

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,32 +1,10 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useState } from "react";
-import Login from "./login";
-import Home from "./home";
-import Board from "./boardscreen";
-import Protected from "./ProtectedRoutes";
-import { useState } from "react";
-
-
-const App = () => {
-  const [isAuthorized, setIsAuthroized] = useState(null)
-const logIn = () => {
-  isAuthorized(true)
-}
-
-const logOut = () => {
-  isAuthorized(false)
-}
+const App = (props) => {
   // TODO: add authentication here
 
   return (
-      <BrowserRouter>
-          <Routes>
-            <Route exact path="/" Component={Login} />
-            <Route path="/home" Component={Home} />
-            <Route path="/board" Component={Board}/>
-          </Routes>
-      </BrowserRouter>
+    <div>
+      {props.children}
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./components/home";
 import store from "./reducers";
+import Board from "./components/boardscreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+      <BrowserRouter>
+        <App>
+          <Routes>
+            <Route exact path="/" Component={Login} />
+            <Route path="/home" Component={Home} />
+            <Route path="/board" Component={Board}/>
+          </Routes>
+        </App>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
