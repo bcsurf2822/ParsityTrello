45a724695db4b7b6fdded9e1b9b913d04029c92b
unknown <nickscape96@gmail.com>
2023-04-07 15:00:49 -0400

    Fixed Generate-boards

--- frontend/src/components/commentComponent.js
+++ frontend/src/components/commentComponent.js
@@ -5,7 +5,7 @@ const commentComponent = ({ comment }) => {
     <div className="w-full flex mt-2 mb-4">
       <img src={Avatar} alt="plussvg" className="object-contain w-12 mr-4" />
       <div className="w-full">
-        <p className="text-sm font-semibold mb-1">{comment.user.username}</p>
+        <p className="text-sm font-semibold mb-1">{comment.users.username}</p>
         <div className="text-sm bg-gray-100 rounded-sm px-2 py-2">
           {comment.comment}
         </div>
