76124794a4b532264f07f376acef88c7f7c43bce
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-04-10 08:10:59 -0400

    fixed card Detail error

--- frontend/src/components/cardComponent.js
+++ frontend/src/components/cardComponent.js
@@ -44,10 +44,7 @@ const CardComponent = ({ card, index, listId }) => {
   const {id} = useParams();
   const boardId = id;
 
-  const cardDetail = () => {
-    openModal();
-  };
-  
+
 
   const addComment = () => {
     dispatch(postComment(listId, cardId, comment, userId));
